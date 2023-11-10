8d1ae65bcfaabeef20acb2007fbb28c4911a581c
tamaragrafskaya <toma.oshea@mail.ru>
2023-11-10 18:52:24 +0300

    Урок 19 Операторы языка.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-console.log('Hello World');
-let username = prompt('Введите ваше имя');
-alert(`Привет, ` + username + ` !`);
+const buttonPlus = document.querySelector('.plus');
+const resultEl = document.querySelector('.result');
+let counter = 0;
+resultEl.textContent = counter;
+buttonPlus.addEventListener('click', function (e) {
+    counter += 3;
+    resultEl.textContent = counter;
+});
